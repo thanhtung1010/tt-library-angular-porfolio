07a5c3240a85b2132e1f5a7134742d6b12918336
tung trinh <tung.trinh@bstarsolutions.com>
2024-04-01 16:51:24 +0700

    update lib config

--- projects/tt-library-angular-porfolio/src/lib/_modules/shared/_pipes/assets-link.pipe.ts
+++ projects/tt-library-angular-porfolio/src/lib/_modules/shared/_pipes/assets-link.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { ASSETS_TYPE } from "../../../_types";
 import { AppConfigService } from "../_services";
+import { ASSETS_TYPE } from "../_types";
 
 @Pipe({
   name: 'TTAssetsLink',
