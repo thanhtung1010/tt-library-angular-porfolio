7197669c82527ee8ac0836e9ec3d2ed121471651
tung trinh <tung.trinh@bstarsolutions.com>
2024-05-29 17:28:12 +0700

    update firebase service

--- projects/tt-library-angular-porfolio/src/lib/_modules/shared/_services/firebase.service.ts
+++ projects/tt-library-angular-porfolio/src/lib/_modules/shared/_services/firebase.service.ts
@@ -5,6 +5,7 @@ import { Observable, Subscriber } from "rxjs";
 import { IAppConfig, IFirebaseConfig } from "../../../_interfaces";
 import { AppConfigService } from ".";
 import { APP_CONFIG_TOKEN } from "../../../_enums";
+import { getAuth, Auth } from "firebase/auth";
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ import { APP_CONFIG_TOKEN } from "../../../_enums";
 
 export class FirebaseService {
   firebaseApp?: FirebaseApp;
+  auth?: Auth;
 
   constructor(
     @Inject(APP_CONFIG_TOKEN) private appConfig: IAppConfig,
@@ -24,6 +26,17 @@ export class FirebaseService {
         throw new Error('firebase config is invalid');
       }
       this.firebaseApp = initializeApp(this.appConfig.firebaseConfig);
+    } catch (error) {
+      throw new Error(JSON.stringify(error));
+    }
+  }
+
+  initAuth() {
+    try {
+      if (!this.firebaseApp) {
+        throw new Error('firebase app is invalid');
+      }
+      this.auth = getAuth(this.firebaseApp);
     } catch (error) {
       throw new Error(JSON.stringify(error));
     }
